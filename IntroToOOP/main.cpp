8e6174fd11c7e6b27a23f6bed8b5d84822125a6a
vivivych <ivychhh@yandex.ru>
2025-05-31 14:00:31 +0300

    intro

--- IntroToOOP/main.cpp
+++ IntroToOOP/main.cpp
@@ -1,65 +1,103 @@
 #include<iostream>
 using namespace std;
 
-struct Point
-{
-	double x;
-	double y;
-
-	//Создавая структуру или класс мы создаем новый тип данных. Класс и структура это тип данных.
-};
-
+//Создавая структуру или класс мы создаем новый тип данных.
+//КЛАСС - это тип данных	
+//СТРУКТУРА - это тип данных	
 class Point
 {
 	double x;
 	double y;
-
 public:
-	double get_x()const   //Метод get нужно делать константным. 
-		         
+	double get_x()const
 	{
 		return x;
 	}
 	double get_y()const
 	{
-
 		return y;
-  }
+	}
 	void set_x(double x)
 	{
-
-		this->x = x;   // This-> это указатель на объект для которого вызывается метод.
+		this->x = x;
 	}
-
 	void set_y(double y)
-
 	{
 		this->y = y;
-
 	}
 
+	//					Constructors:
+	/*Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(int x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "Constructor:\t\t" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << "X = " << get_x() << "\tY = " << get_y() << endl;
+	}
 };
+
 //#define STRUCT_POINT
+//#define OBJECT_LIFETIME
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
-
-
-
 #ifdef STRUCT_POINT
-	Point A; // "объявление переменной типа Point"
-	//создание объекта/экземпляра А структуры Point
-
+	int a;		//Объявление переменной a типа int
+	Point A;	//Объявление переменной A типа Point
+	//Создание объекта/экземпляра 'A' структуры 'Point' !!
 	A.x = 2;
 	A.y = 3;
-
 	cout << A.x << "\t" << A.y << endl;
 
 	Point* pA = &A;
-
 	cout << pA->x << "\t" << pA->y << endl;
-#endif
+#endif // STRUCT_POINT
+
+	Point A;	//Здесь неявно вызывается конструктор по умолчанию,поскольку мы создаем объект "По умолчанию"
+//A.set_x(2);
+//A.set_y(3);
+//cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+
+	Point B = 5;
+	//cout << B.get_x() << "\t" << B.get_y() << endl;
+	B.print();
+
+	Point C(7, 8);
+	C.print();
+
+
+#ifdef OBJECT_LIFETIME
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << "\t";
+	}
+	
+	cout << endl;
+#endif // OBJECT_LIFETIME
+
 	/*
 	
 	Доступ к полям объектов
