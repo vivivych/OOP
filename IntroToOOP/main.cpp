1385e58169b0e4bc51ef02125b7f1f4db9eff41c
vivivych <ivychhh@yandex.ru>
2025-06-12 02:02:17 +0300

    distance

--- IntroToOOP/main.cpp
+++ IntroToOOP/main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 
-//Создавая структуру или класс мы создаем новый тип данных.
-//КЛАСС - это тип данных	
-//СТРУКТУРА - это тип данных	
+#define delimiter "\n------------------------------------\n"
+
+/*
+Создавая структуру или класс мы создаем новый тип данных. Класс и структура это тип данных.
+*/
 class Point
 {
 	double x;
@@ -44,29 +46,78 @@ public:
 		this->y = y;
 		cout << "Constructor:\t\t" << this << endl;
 	}
+	//Point(const Point& other) = delete;	//Удаляем конструктор копирования, и таким образом запрещаем копирование объектов.
+	Point(const Point& other)
+	{
+		//this - этот объект
+		//other - тот объект
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	~Point()
 	{
 		cout << "Destructor:\t\t" << this << endl;
 	}
 
-	//				Methods:
+	//					Operators:
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+	}
+
+	Point& operator++()
+	{
+		this->x++;
+		this->y++;
+		return *this;
+	}
+
+	//					Methods:
+	double distance(const Point& other)const
+	{
+		//other.x *= 100;
+		//this->x *= 100;
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);//Square Root, квадратный корень
+		return distance;
+	}
 	void print()const
 	{
 		cout << "X = " << get_x() << "\tY = " << get_y() << endl;
 	}
 };
 
+double distance(const Point& A, const Point& B)
+{
+	//B.set_y(B.get_y() * 200);
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+
 //#define STRUCT_POINT
-//#define OBJECT_LIFETIME
+#define DISTANCE_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
 #ifdef STRUCT_POINT
-	int a;		//Объявление переменной a типа int
-	Point A;	//Объявление переменной A типа Point
-	//Создание объекта/экземпляра 'A' структуры 'Point' !!
+	int a;		//Объявление переменной 'a' типа 'int'
+	Point A;	//Объявление переменной 'A' типа 'Point'
+	//Создание объекта 'A' структуры 'Point'
+	//Создание экземпляра 'A' структуры 'Point'
 	A.x = 2;
 	A.y = 3;
 	cout << A.x << "\t" << A.y << endl;
@@ -75,28 +126,27 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
 
-	Point A;	//Здесь неявно вызывается конструктор по умолчанию,поскольку мы создаем объект "По умолчанию"
-//A.set_x(2);
-//A.set_y(3);
-//cout << A.get_x() << "\t" << A.get_y() << endl;
-	A.print();
 
-	Point B = 5;
-	//cout << B.get_x() << "\t" << B.get_y() << endl;
+#ifdef DISTANCE_CHECK
+	Point A(2, 3);
+	Point B(7, 8);
+	A.print();
 	B.print();
 
-	Point C(7, 8);
-	C.print();
+	cout << delimiter << endl;
+	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками 'A' и 'B':   " << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками 'B' и 'A':   " << distance(B, A) << endl;
+	cout << delimiter << endl;
+	cout << sizeof(Point) << endl;
+
+#endif // DISTANCE_CHECK
 
 
-#ifdef OBJECT_LIFETIME
-	for (int i = 0; i < 10; i++)
-	{
-		cout << i << "\t";
-	}
-	
-	cout << endl;
-#endif // OBJECT_LIFETIME
 
 	/*
 	
@@ -123,4 +173,29 @@ protected:     - защищенные поля, доступны внутри класса и дочерних классов. Это
 	 set - открывает доступ к закрытым переменным класса на ЗАПИСЬ. (Позволяет задать значение закрытых переменных)
 	*/
 
+/*Constructor - это метод, который создает объект.
+	-По умолчанию - это конструктор, который может быть вызван без параметров;
+	-Без параметров;
+	-С параметрами;
+	-Конструктор копирования;
+	-Конструктор переноса;
+~Destructor - это метод, который уничтожает объект по завершении его времени жизни;*/
+
+/*
+			Overloading rules
+1. Перегрузить можно только существующие операторы,
+   создать новые операторы невозможно:
+	+  - перегружается;
+	++ - перегружается;
+	%  - перегружается;
+	%% - НЕ перегружается;
+
+2. Перегружить можно НЕ все сущкствующие операторы,
+   НЕ перегружаются:
+	?: - Conditional Ternary Operator;
+	:: - Оператор разрешения видимости (Scope operator);
+	.  - Оператор прямого доступа (Point operator);
+	.* - Pointer to member selection;
+	#  - Preprocessor convert to string;
+	## - Preprocessor concatenate;*/
 }
